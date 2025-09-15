5609679984aee4182d1012f00f0af780a79cf4b0
kogarashihiro <kogarashihiro@gmail.com>
2025-09-15 13:27:59 +0700

    Array
    Syntax Array Dasar

---
+++ 1 variabel/latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Variabel</title>
+</head>
+<body>
+	<?php
+		$nama="Alfino Putra Arisandi";
+
+		echo $nama;
+	?>
+</body>
+</html>
---
+++ 1 variabel/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Variabel Latihan 2</title>
+</head>
+<body>
+	<?php
+		$nama="Alfino Putra Arisandi";
+		$kelas="11 RPL";
+		$alamat="Jl. Bagus Yabin no 56, Cigadung, Kab.Subang";
+
+		echo "Nama : $nama";
+		echo "<br>";
+		echo "Kelas : $kelas";
+		echo "<br>"; 
+		echo "Alamat : $alamat"
+ 	?>
+</body>
+</html>
